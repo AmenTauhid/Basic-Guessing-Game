90494b690a1d9737a6a246d4f2b39deecb8e467f
Amen <aymantauhid2@gmail.com>
2022-02-06 00:41:06 +0530

    Add project files.

---
+++ W4A_GuessingGame/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace W4A_GuessingGame
+{
+    public enum GuessResult
+    {
+        TooHigh,
+        TooLow,
+        JustRight
+    }
+    public class Game
+    {
+        private int _productPrice;
+        public int Counter;
+        public Game()
+        {
+            Random randomGenerator = new Random();
+            _productPrice = randomGenerator.Next(0, 1001);
+            Counter = 0;
+        }
+
+        public GuessResult CheckGuess(int userGuess)
+        {
+            Counter++;
+            if (userGuess > _productPrice)
+            
+                return GuessResult.TooHigh;
+            
+            else if (userGuess < _productPrice)
+            
+                return GuessResult.TooLow;
+            
+            else
+            
+                return GuessResult.JustRight;
+
+        }
+    }
+}
---
+++ W4A_GuessingGame/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace W4A_GuessingGame
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            StartGame();
+        }
+
+        private Game _game;
+
+        private void StartGame()
+        {
+            _game = new Game();
+            messageLabel.Text = "Guess a number between 1 and 1000";
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            try
+            {
+                int userGuess = int.Parse(inputEntry.Text);
+                GuessResult result= _game.CheckGuess(userGuess);
+                if (result == GuessResult.TooHigh)
+                {
+                    resultLabel.Text = "Too high!";
+                }
+                else if (result == GuessResult.TooLow)
+                {
+                    resultLabel.Text = "Too low!";
+                }
+                else
+                {
+                    resultLabel.Text = $"That's the right answer! You tried {_game.Counter} times";
+                }
+            }
+            catch (Exception exception)
+            {
+                resultLabel.Text = "Invalid Input";
+            }
+        }
+    }
+}
